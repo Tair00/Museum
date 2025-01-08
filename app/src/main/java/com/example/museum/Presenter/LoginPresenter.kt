fe560cc97399421e17e265bd669905bc701012e3
Tair00 <suleimanov.t.i.20@gmail.com>
2025-01-08 12:03:25 +0300

    Экран авторизации

---
+++ app/src/main/java/com/example/museum/Presenter/LoginPresenter.kt
@@ -0,0 +1,26 @@
+package com.example.museum.Presenter
+
+import com.example.museum.Domain.LoginUseCase
+import com.example.museum.Model.LoginRequest
+import com.example.museum.Model.LoginResult
+import com.example.museum.View.LoginView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class LoginPresenter(
+    private val loginUseCase: LoginUseCase,
+    private val view: LoginView
+) {
+    fun onLoginButtonClicked(usernameOrEmail: String, password: String) {
+        view.showLoading()
+        CoroutineScope(Dispatchers.Main).launch {
+            val result = loginUseCase.execute(LoginRequest(usernameOrEmail, password))
+            view.hideLoading()
+            when (result) {
+                is LoginResult.Success -> view.navigateToHomeScreen()
+                is LoginResult.Error -> view.showError(result.message)
+            }
+        }
+    }
+}
