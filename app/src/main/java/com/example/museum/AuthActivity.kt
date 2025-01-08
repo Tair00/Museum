27f55395eab66843894d28a012a75298095fa9a1
Tair00 <suleimanov.t.i.20@gmail.com>
2025-01-08 12:03:08 +0300

    Экран авторизации

--- app/src/main/java/com/example/museum/AuthActivity.kt
+++ app/src/main/java/com/example/museum/AuthActivity.kt
@@ -1,15 +1,13 @@
 package com.example.museum
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_registration)
 
     }
